aa3e6064e9baddd4a73e5866b58e982500d07dd0
harshjaat98 <97422757+harshjaat98@users.noreply.github.com>
2023-06-07 21:52:22 +0530

    Update upload.sh

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -23,31 +23,11 @@ echo "Uploading the Build..."
 echo "============================"
 
 # Change to the Output Directory
-cd out/target/product/${DEVICE}
+cd out/target/product/RMX2001
 
-# Set FILENAME var
-FILENAME=recovery.img
-FILENAME2=boot.img
+curl -T recovery.img temp.sh
 
-# Upload to oshi.at
-if [ -z "$TIMEOUT" ];then
-    TIMEOUT=20160
-fi
-
-# Upload to WeTransfer
-# NOTE: the current Docker Image, "registry.gitlab.com/sushrut1101/docker:latest", includes the 'transfer' binary by Default
-transfer wet $FILENAME > link.txt || { echo "ERROR: Failed to Upload the Build!" && exit 1; }
-transfer wet $FILENAME2 > link2.txt || { echo "ERROR: Failed to Upload the Build!" && exit 1; }
-
-
-DL_LINK=$(cat link.txt | grep Download | cut -d\  -f3)
-DL_LINK2=$(cat link2.txt | grep Download | cut -d\  -f3)
-
-# Show the Download Link
-echo "=============================================="
-echo "Download Link: ${DL_LINK}" || { echo "ERROR: Failed to Upload the Build!"; }
-echo "Download Link: ${DL_LINK2}" || { echo "ERROR: Failed to Upload the Build!"; }
-echo "=============================================="
+link=curl -T recovery.img temp.sh
 
 DATE_L=$(date +%d\ %B\ %Y)
 DATE_S=$(date +"%T")
@@ -59,8 +39,8 @@ echo -e \
 ✅ Build Completed Successfully!
 📱 Device: "${DEVICE}"
 🖥 Build System: "${TWRP_BRANCH}"
-⬇️ Download Link: <a href=\"${DL_LINK}\">Here</a>
-⬇️ Download Link: <a href=\"${DL_LINK2}\">Here</a>
+⬇️ Download Link: <a href=\"${link}\">Here</a>
+
 📅 Date: "$(date +%d\ %B\ %Y)"
 ⏱ Time: "$(date +%T)"
 " > tg.html
