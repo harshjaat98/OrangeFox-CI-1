c6a25360cb4c4fa34fc079ac5a759284c48c6c85
harshjaat98 <97422757+harshjaat98@users.noreply.github.com>
2023-06-13 22:57:23 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 # Device
 export TWRP_BRANCH="shrp-12.1"
-export DT_LINK="https://github.com/EvilAnsh/twrp_device_realme_RMX2151.git -b android-12.1"
+export DT_LINK="https://github.com/harshjaat98/twrp_device_realme_RMX2001 -b android-12.1"
 
 export DEVICE="RMX2151"
 export OEM="realme"
